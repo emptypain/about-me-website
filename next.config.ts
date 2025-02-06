df91b81eb1d366ce02c56cab4cb4545536c46762
kokos <ya.ploxoi82@mail.ru>
2025-02-06 09:53:36 +0500

    feat: used settings for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: '/about-me-website',
+  assetPrefix: '/about-me-website',
+  images: {
+    unoptimized: true, // Отключаем оптимизацию изображений, т.к. она требует серверной обработки
+  },
 };
 
 export default nextConfig;
