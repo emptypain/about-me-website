0612449964322d7e9eaa4041ce1ba26b01b5eb78
kokos <ya.ploxoi82@mail.ru>
2025-02-06 11:27:37 +0500

    feat: добавил поля в некст конфиг

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export',
+  output: 'export',
   // basePath: '/about-me-website',
   // assetPrefix: '/about-me-website/',
-  // trailingSlash: true,
+  trailingSlash: true,
   // images: {
   //   unoptimized: true, // Отключаем оптимизацию изображений, т.к. она требует серверной обработки
   // },
